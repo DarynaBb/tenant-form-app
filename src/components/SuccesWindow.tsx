b6d0deea061fb531b5bff3352a77162e22f1e39d
DarynaBb <darinabilenko@gmail.com>
2024-06-15 20:32:45 +0200

    succes winwdow styled

--- src/components/SuccesWindow.tsx
+++ src/components/SuccesWindow.tsx
@@ -2,7 +2,7 @@
 
 function SuccesWindow() {
   return (
-    <div className="text-[4em] h-screen text-gray-700 open-sans flex flex-col items-center justify-center">
+    <div className="text-[2em] md:text-[3em] lg:text-[4em] h-screen text-gray-700 open-sans flex flex-col items-center justify-center">
         <p>Thank You!</p>
         <p>We will contact you shortly :)</p>
     </div>
